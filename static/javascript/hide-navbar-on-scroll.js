de13925515db604c4b53aca9ed94018b60e4487f
TadeoRiganti <tadeoriganti@gmail.com>
2025-10-23 23:17:19 -0300

    REFACTOR Change method to class and add a comment

--- static/javascript/hide-navbar-on-scroll.js
+++ static/javascript/hide-navbar-on-scroll.js
@@ -3,9 +3,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementByClassName("primary-header").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementByClassName("primary-header").style.top = "4rem"; /*For now I'll let hardcoded*/ */
   }
   prevScrollpos = currentScrollPos;
 }
