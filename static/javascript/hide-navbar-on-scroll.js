6c6b90bebba5c4877e4a3267034e2d9b952452ce
TadeoRiganti <tadeoriganti@gmail.com>
2026-01-06 00:20:38 -0300

    REFACTOR hide-navbar-on-scroll.js
    SCOPE: When scrolling up, navbar partially hides
    the aside sections.
    Add some logic to swap height variables of the aside
    elements so it shift away when navbar appears,
    leaving enough space to show itself without hiding
    the other elemnts.

--- static/javascript/hide-navbar-on-scroll.js
+++ static/javascript/hide-navbar-on-scroll.js
@@ -7,10 +7,17 @@ Source: https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp*/
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
+  var navbar = document.getElementById("navbar");
+  var toc = document.getElementById("toc"); // your aside TOC element
+
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    // scrolling up → show navbar
+    navbar.style.top = "0";
+    toc.style.top = "var(--spacing-extra-large)";
   } else {
-    document.getElementById("navbar").style.top = `-${navbar.offsetHeight}px`; 
+    // scrolling down → hide navbar
+    navbar.style.top = `-${navbar.offsetHeight}px`;
+    toc.style.top = "var(--spacing-large)";
   }
   prevScrollpos = currentScrollPos;
 }
