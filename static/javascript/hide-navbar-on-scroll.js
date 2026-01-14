e259a0905f323b5591afc49a52252ebf6b392bb8
TadeoRiganti <tadeoriganti@gmail.com>
2026-01-13 21:09:48 -0300

    REFACTOR Remove aside references

--- static/javascript/hide-navbar-on-scroll.js
+++ static/javascript/hide-navbar-on-scroll.js
@@ -1,10 +1,31 @@
+/* Version 2:
+Script to hide the navbar when scrolling down and show it when scrolling up */
 
-/* HIDE NAVBAR ON SCROLL 
+var prevScrollpos = window.pageYOffset;
 
-Source: https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp*/
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  var navbar = document.getElementById("navbar");
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-/* HIDE NAVBAR ON SCROLL 
+  if (prevScrollpos > currentScrollPos) {
+    // scrolling up → show navbar
+    navbar.style.top = "0";
+  } else {
+    // scrolling down → hide navbar
+    navbar.style.top = `-${navbar.offsetHeight}px`;
+  }
+
+  prevScrollpos = currentScrollPos;
+};
+
+/* REFACTOR Control animation via CSS
+Scope: Refactored navbar scroll script: replaced inline style.top assignments with class toggling (.visible),
+allowing CSS to handle animation, delay, and easing instead of JS directly.*/
+
+// ----------------------------------------
+
+/* Version 1
+When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
 Source: https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp */
 
 // var prevScrollpos = window.pageYOffset;
@@ -28,26 +49,3 @@ Source: https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp */
 //   }
 //   prevScrollpos = currentScrollPos;
 // }
-
-// REFACTOR Control animation via CSS
-// Scope: Refactored navbar scroll script: replaced inline style.top assignments with class toggling (.visible),
-// allowing CSS to handle animation, delay, and easing instead of JS directly.
-
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  var navbar = document.getElementById("navbar");
-  var asides = document.querySelectorAll("aside");
-
-  if (prevScrollpos > currentScrollPos) {
-    // scrolling up → show navbar
-    navbar.classList.add("visible");
-    asides.forEach(aside => aside.classList.add("visible"));
-  } else {
-    // scrolling down → hide navbar
-    navbar.classList.remove("visible");
-    asides.forEach(aside => aside.classList.remove("visible"));
-  }
-  prevScrollpos = currentScrollPos;
-};
