83076a39c06b965233c67fc868839b93239dcbc8
TadeoRiganti <tadeoriganti@gmail.com>
2026-01-22 14:12:21 -0300

    REFACTOR Move dark-mode button to primary-header

--- static/javascript/Experimental/color-theme-mode.js
+++ static/javascript/Experimental/color-theme-mode.js
@@ -1,15 +1,16 @@
+
 function toggleTheme() {
-            const html = document.documentElement;
-            const button = document.querySelector('.theme-toggle');
-            const currentTheme = html.getAttribute('data-theme');
-            
-            if (currentTheme === 'light') {
-                html.setAttribute('data-theme', 'dark');
-                button.setAttribute('aria-pressed', 'true');
-                button.setAttribute('aria-label', 'Toggle theme (dark mode active)');
-            } else {
-                html.setAttribute('data-theme', 'light');
-                button.setAttribute('aria-pressed', 'false');
-                button.setAttribute('aria-label', 'Toggle theme (light mode active)');
-            }
-        }
+    const html = document.documentElement;
+    const button = document.querySelector('.primary-header__theme-toggle');
+    const currentTheme = html.getAttribute('data-theme');
+
+    if (currentTheme === 'light') {
+        html.setAttribute('data-theme', 'dark');
+        button.setAttribute('aria-pressed', 'true');
+        button.setAttribute('aria-label', 'Toggle theme (dark mode active)');
+    } else {
+        html.setAttribute('data-theme', 'light');
+        button.setAttribute('aria-pressed', 'false');
+        button.setAttribute('aria-label', 'Toggle theme (light mode active)');
+    }
+}
